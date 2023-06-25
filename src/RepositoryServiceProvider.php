5a23f38b50086214e83f263d1c2fe876cd786443
ashrafAli23 <ashrafammar230@gmail.com>
2023-06-25 10:54:57 +0300

    fix bug

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -8,10 +8,6 @@ use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
-    protected $commands = [
-        \Laravel\Repository\Commands\CreateRepositoryCommand::class,
-    ];
-
     public function boot()
     {
         if ($this->app->runningInConsole()) {
